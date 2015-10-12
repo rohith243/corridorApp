56203d028e7e76620689bacfac11714a0629f718
Kiran <akiran547@gmail.com>
2015-10-12 15:51:17 +0530

    updated mail template

--- routes/modules/mail.js
+++ routes/modules/mail.js
@@ -27,10 +27,10 @@ mail.send = function  ( item, user ) {
     console.log( 'sending mails' );
     mailOptions.to = item.owner.mail;
     mailOptions.cc = user.mail;
-    mailOptions.subject = 'Letsbuild Notification: Interest ' + item.appName;
-    mailOptions.html = '<p>Hi '+ item.owner.uid +'</p>';
-    mailOptions.html += '<p>'+ user.uid +' had expressed Interest in the app</p>';
-    mailOptions.html += '<p><b>Cotribution Hours :</b> '+ user.hours +'</p>';
+    mailOptions.subject = 'LetsBuild Notification: Interest ' + item.appName;
+    mailOptions.html = '<p>Hi '+ ( item.owner.uid )+'</p>';
+    mailOptions.html += '<p><b>'+ ( user.fullName || user.uid ) +'</b> has expressed interest in the app</p>';
+    mailOptions.html += '<p><b>Contribution Hours :</b> '+ user.hours +'</p>';
     mailOptions.html += '<p><b>Comments: </b>'+ user.aboutme +'</p>';
     transporter.sendMail(mailOptions, function(error, info){
         if(error) {
