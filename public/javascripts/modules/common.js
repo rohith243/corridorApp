c7af29b23e3bc58970a6cae7f1cd06495116b6ab
Kiran <akiran547@gmail.com>
2015-10-12 23:56:11 +0530

    updated changes for images loading

--- public/javascripts/modules/common.js
+++ public/javascripts/modules/common.js
@@ -1,8 +1,16 @@
 (function(angular) {
+    if(typeof path === 'undefined' ) {
+        path = '';
+    }
     var tempImages = {
-        'kiran.adepu@imaginea.com': 'tempImage',
-        'kiran.adepu@imaginea1.com': 'tempImage22'
+        'apurba.n@imaginea.com':  '/images/apurba.jpg',
+        'jay.pullur@pramati.com': '/images/jay.jpg',
+        'vijay@pramati.com': '/images/vijay.jpg',
+        'chandru@pramati.com':'/images/chandru.jpg',
+        'sharad.s@imaginea.com':'/images/sharad.jpg',
+        'kvp@pramati.com': '/images/kvp.jpg'
     };
+
     angular.module('commonModule', ['ngMaterial', 'httpService', 'globalNavigation','headroom'])
         .factory('model', [function() {
             var data = {};
@@ -10,6 +18,9 @@
         }])
         .filter('getImages', function() {
           return function( inputArray ) {
+            if( !inputArray ) {
+                return []
+            }
             for( var i = 0; i < inputArray.length; i++ ) {
                 if( tempImages[ inputArray[i].mail ] ) {
                     inputArray[i].src = tempImages[ inputArray[i].mail ];
