6bfd6f6fe660c3371d0232ee4f1c016fa2e32855
PrashanthAmbure <p.soccer10@gmail.com>
2015-10-12 16:12:22 +0530

    Typo errors resolved.

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var user = require('./modules/user');
 router.get('/', function(req, res, next) {
     res.render('gallery', {
-        title: 'Gallery | Letsbuild',
+        title: 'Gallery | LetsBuild',
         user: user.getDetails(req)
     });
 });
 router.get('/id/:_id', function(req, res, next) {
     res.render('app-details', {
-        title: 'App Details | Letsbuild',
+        title: 'App Details | LetsBuild',
         user: user.getDetails(req)
     });
 });
