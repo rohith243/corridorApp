5555b4c0a8cace5c00ce50fd589a9eb99c50b849
Kiran <akiran547@gmail.com>
2015-11-25 16:36:55 +0530

    removed https implementation

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
 var session = require('express-session');
-var http = require('http');
-var https = require('https');
-var sslConfig = require('./ssl-config');
 var app = module.exports = loopback();
 
 var cas = require('connect-cas');
@@ -33,35 +30,25 @@ app.use(function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.start = function(httpOnly) {
-  if(httpOnly === undefined) {
-    httpOnly = process.env.HTTP;
-  }
-  var server = null;
-  if(!httpOnly) {
-    var options = {
-      key: sslConfig.privateKey,
-      cert: sslConfig.certificate
-    };
-    server = https.createServer(options, app);
-  } else {
-    server = http.createServer(app);
-  }
-  server.listen(app.get('port'), function() {
-    var baseUrl = (httpOnly? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
-    app.emit('started', baseUrl);
-    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
-  return server;
 };
 
-// boot scripts mount components like REST API
 boot(app, __dirname, function(err) {
   if (err) throw err;
   // start the server if `$ node server.js`
   if (require.main === module)
     {
       var appSocket = require( './app-socket' );
-      appSocket.init( app, app.start( true ) );      
+      appSocket.init( app, app.start() );      
     }
 });
